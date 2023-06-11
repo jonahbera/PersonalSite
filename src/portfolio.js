953fb0d7657a57dce3f5866b846c1877857cce43
jonahbera <jonahbera@gmail.com>
2023-06-11 16:01:46 -0500

    K

---
+++ src/portfolio.js
@@ -0,0 +1,594 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: false, // Change this to false if you want the good'ol cursor
+  googleTrackingID: "UA-174238252-2",
+};
+
+//Home Page
+const greeting = {
+  title: "Hello 👋.",
+  title2: "Jonah",
+  logo_name: "Jonah Bera",
+  // nickname: "harry / picleric",
+  full_name: "Jonah Bera",
+  subTitle:
+    "Software Engineer, Life Long Learner",
+  resumeLink:
+    "Jonah_s_Resume_2023.pdf",
+  mail: "mailto: jonahbera@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/jonahbera",
+  linkedin: "https://www.linkedin.com/in/jonah-bera/",
+  gmail: "jonahbera@gmail.com",
+  // gitlab: "https://gitlab.com/harikanani",
+  // facebook: "https://www.facebook.com/vedansh.vijaywargiya/",
+  // twitter: "https://twitter.com/vedansh_v_/",
+  // instagram: "https://www.instagram.com/vedv_2002/",
+};
+
+const skills = {
+  data: [
+    {
+      title: "My Skills",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly efficient applications",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "simple-icons:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+        // {
+        //   skillName: "Bootstrap",
+        //   fontAwesomeClassname: "simple-icons:bootstrap",
+        //   style: {
+        //     color: "#563d7c",
+        //   },
+        // },
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:node-dot-js",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "simple-icons:npm",
+          style: {
+            color: "#CB3837",
+          },
+        },
+        // {
+        //   skillName: "Flask",
+        //   fontAwesomeClassname: "simple-icons:flask",
+        //   style: {
+        //     color: "#000000",
+        //   },
+        // },
+        // {
+        //   skillName: "Django",
+        //   fontAwesomeClassname: "simple-icons:django",
+        //   style: {
+        //     color: "#0c4b33",
+        //   },
+        // },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+        {
+          skillName: "GraphQL",
+          fontAwesomeClassname: "simple-icons:graphql",
+          style: {
+            color: "#DE33A6",
+          },
+        },
+        // {
+        //   skillName: "Android",
+        //   fontAwesomeClassname: "simple-icons:android",
+        //   style: {
+        //     color: "#3DDC84",
+        //   },
+        // },
+        {
+          skillName: "Java",
+          fontAwesomeClassname: "simple-icons:java",
+          style: {
+            color: "#f89820",
+          },
+        },
+        // {
+        //   skillName: "Flutter",
+        //   fontAwesomeClassname: "simple-icons:flutter",
+        //   style: {
+        //     color: "#02569B",
+        //   },
+        // },
+        // {
+        //   skillName: "Dart",
+        //   fontAwesomeClassname: "simple-icons:dart",
+        //   style: {
+        //     color: "#29B0EE",
+        //   },
+        // },
+        {
+          skillName: "Linux",
+          fontAwesomeClassname: "simple-icons:linux",
+          style: {
+            color: "#000000",
+          },
+        },
+        // {
+        //   skillName: "MySQL",
+        //   fontAwesomeClassname: "simple-icons:mysql",
+        //   style: {
+        //     color: "#4479A1",
+        //   },
+        // },
+        // {
+        //   skillName: "jQuery",
+        //   fontAwesomeClassname: "simple-icons:jquery",
+        //   style: {
+        //     color: "#0865A6",
+        //   },
+        // },
+        // {
+        //   skillName: "Wordpress",
+        //   fontAwesomeClassname: "simple-icons:wordpress",
+        //   style: {
+        //     color: "#207297",
+        //   },
+        // },
+        // {
+        //   skillName: "Apache",
+        //   fontAwesomeClassname: "simple-icons:apache",
+        //   style: {
+        //     color: "#CA1A22",
+        //   },
+        // },
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "C",
+          fontAwesomeClassname: "simple-icons:c",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        // {
+        //   skillName: "C++",
+        //   fontAwesomeClassname: "simple-icons:cplusplus",
+        //   style: {
+        //     color: "#E94E32",
+        //   },
+        // },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      title: "Cloud Infra-Architecture",
+      fileName: "CloudInfraImg",
+      skills: [
+        "⚡ Experience working on multiple cloud platforms",
+        "⚡ Experience hosting and managing websites",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "AWS",
+          fontAwesomeClassname: "simple-icons:amazonaws",
+          style: {
+            color: "#FF9900",
+          },
+        },
+        // {
+        //   skillName: "Netlify",
+        //   fontAwesomeClassname: "simple-icons:netlify",
+        //   style: {
+        //     color: "#38AFBB",
+        //   },
+        // },
+        {
+          skillName: "Heroku",
+          fontAwesomeClassname: "simple-icons:heroku",
+          style: {
+            color: "#6863A6",
+          },
+        },
+        {
+          skillName: "Firebase",
+          fontAwesomeClassname: "simple-icons:firebase",
+          style: {
+            color: "#FFCA28",
+          },
+        },
+        // {
+        //   skillName: "PostgreSQL",
+        //   fontAwesomeClassname: "simple-icons:postgresql",
+        //   style: {
+        //     color: "#336791",
+        //   },
+        // },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+          },
+        },
+        {
+          skillName: "Docker",
+          fontAwesomeClassname: "simple-icons:docker",
+          style: {
+            color: "#1488C6",
+          },
+        },
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "University Of Wisconsin-Madison",
+      subtitle: "Bachelors of Science in Computer Science ",
+      logo_path: "w.png",
+      alt_name: "SSEC",
+      duration: "2021-2024",
+      descriptions: [
+        "⚡  I'm currently pursuing my Bachelors in Computer Science .",
+        "⚡ I have studied core subjects like Data Structures, DBMS, Algorithms and Operating Systems",
+        // "⚡ I have also completed various online courses for Backend , Web , Mobile App Development, etc.",
+        // "⚡ I have implemented several projects based on what I've leart under my Computer Engineering course. ",
+      ],
+      website_link: "https://www.cs.wisc.edu/",
+    },
+  ],
+};
+
+const certifications = {
+  certifications: [
+    {
+      title: "Google Cloud",
+      subtitle: "Google Cloud Training",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1r0hYt8Pp0N0u_4Tg-poMF1Qv3LkoKVAP/view",
+      alt_name: "MongoDB University",
+      // color_code: "#2AAFED",
+      color_code: "#2AAFED",
+    },
+    {
+      title: "Hackathon",
+      subtitle: "Backyard Hacks 2.0",
+      logo_path: "hackathon1.png",
+      // certificate_link:
+      //   "https://drive.google.com/file/d/1Y-HT1flWDfeyfnfqVr3ubNgGIcnuUEYt/view?usp=sharing",
+      alt_name: "hackathon",
+      color_code: "#f47e2c",
+    },
+    {
+      title: "Hackathon",
+      subtitle: "HyperHacks",
+      logo_path: "hackathon2.png",
+      // certificate_link:
+      //   "https://drive.google.com/file/d/1Y-HT1flWDfeyfnfqVr3ubNgGIcnuUEYt/view?usp=sharing",
+      alt_name: "hackathon",
+      color_code: "#E2405F",
+    },
+    // {
+    //   title: "A300: Atlas Security",
+    //   subtitle: "MongoDB University",
+    //   logo_path: "mongo.png",
+    //   certificate_link:
+    //     "https://drive.google.com/file/d/12u_tkvhgB-K2TIm-RDdJOwYbQ9ccqiqA/view?usp=sharing",
+    //   alt_name: "MongoDB University",
+    //   // color_code: "#F6B808",
+    //   color_code: "#47A048",
+    // },
+    {
+      title: "Contributor's Hack",
+      subtitle: "Hakin Codes",
+      logo_path: "HakinCodes.jfif",
+      certificate_link:
+        "https://drive.google.com/file/d/1xvyy1M05lQN-s2VKw_Qvz6E7rPwrK2qm/view?usp=sharing",
+      alt_name: "Opensource Event",
+      color_code: "#fefd7b",
+    },
+    // {
+    //   title: "MLH Local Hack Day: Build",
+    //   subtitle: "Major League Hacking",
+    //   logo_path: "mlh-logo.svg",
+    //   certificate_link:
+    //     "https://drive.google.com/file/d/1ws7g2Oepu76vDFir6SvR_emFAMSkB2ZQ/view?usp=sharing",
+    //   alt_name: "Google",
+    //   color_code: "#fe0037",
+    // },
+    {
+      title: "Olympiad",
+      subtitle: "Ramanujan Mathematics Olympiad",
+      logo_path: "Olympiad.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1SAKHjKisJIY3-FRZQGKLusdsGDusXAjX/view",
+      alt_name: "Postman",
+      // color_code: "#f36c3d",
+      color_code: "#fffbf3",
+    },
+    {
+      title: "Competitive Programming",
+      subtitle: "Reply Challenges",
+      logo_path: "Reply.jfif",
+      certificate_link:
+        "https://drive.google.com/file/d/1KamrXJPTrF3zfMuEerbSnFUjOkt-agKT/view",
+      alt_name: "Reply",
+      color_code: "#2AAFED",
+    },
+    {
+      title: "Competitive Programming",
+      subtitle: "Google Hash Code",
+      logo_path: "google_logo.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1sD6GS8f2Fh5MHMWlf5DhXoMhu4mykW-f/view",
+      alt_name: "Reply",
+      color_code: "#2AAFED",
+    },
+    {
+      title: "Training",
+      subtitle: "30 Days of Open Source",
+      logo_path: "CFC.jfif",
+      certificate_link:
+        "https://drive.google.com/file/d/1SAKHjKisJIY3-FRZQGKLusdsGDusXAjX/view",
+      alt_name: "Postman",
+      // color_code: "#f36c3d",
+      color_code: "#a60000",
+    },
+    {
+      title: "Training",
+      subtitle: "Git Learning",
+      logo_path: "Progate.png",
+      certificate_link:
+        "https://drive.google.com/file/d/1sD6GS8f2Fh5MHMWlf5DhXoMhu4mykW-f/view",
+      alt_name: "Reply",
+      color_code: "#88e7ce",
+    },
+    // color_code: "#8C151599",
+    // color_code: "#7A7A7A",
+    // color_code: "#0C9D5899",
+    // color_code: "#C5E2EE",
+    // color_code: "#ffc475",
+    // color_code: "#g",
+    // color_code: "#ffbfae",
+    // color_code: "#fffbf3",
+    // color_code: "#b190b0",
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "",
+  description:
+    "I've completed two internships. I've done projects on my own, on teams, during hackathons and through my course work.",
+  header_image_path: "experience.svg",
+  sections: [
+    {
+      title: "Work Experience",
+      experiences: [
+        {
+          title: "Software Engineer Intern",
+          company: "Realtor.com",
+          company_url: "https://www.realtor.com/",
+          logo_path: "rdclogo.png",
+          duration: "June 2023 - Current",
+          location: "Chicago, Illinois",
+          description: `Software Engineering Intern on the Growth-New Homes team.
+          `,
+          // "I worked on the Dashboard project which helps users track their activities while using Walo Application. I also worked on Ocean Inventory Application and it's Admin panel Backend as well as on Ocean Inventory Admin Front-end using React and also worked on Walo Admin Backend.",
+          color: "#ee3c26",
+        },
+        {
+          title: "Software Engineer  Intern",
+          company: "Northwestern Mutual",
+          company_url: "https://www.northwesternmutual.com/",
+          logo_path: "nmlogo.avif",
+          duration: "June 2022 - Aug 2022",
+          location: "Milwaukee, Wisconsin",
+          description: `Worked in an Agile environment on the DevOps Engineering team to limit congestion in back-end pipeline operations. Built and enhanced infrastructure through AWS in multiple deployment environments for a policy illustration application utilized by over 10,000 employees. {Utilized Terraform, DynamoDB, Node.js, and Python to create a Unified Dynamic Inventory of 85 department specific services. Implemented CI/CD and other tooling as needed to support enhanced application code infrastructure.
+          `,
+          // "Created Front end of Yearn Financial Mutual Funds website. also degined simple web application for better user experience, designed DB Schemas as well.",
+          color: "#0071C",
+        },
+      ],
+    },
+    // {
+    //   title: "Volunteerships",
+    //   experiences: [
+    //     {
+    //       title: "R&D Member",
+    //       company: "Webarch Club",
+    //       company_url: "https://www.webarchsrm.com/",
+    //       logo_path: "Webarch.jfif",
+    //       duration: "Mar 2020 - Nov 2020",
+    //       location: "SRM IST Kattankulathur",
+    //       description:
+    //         "Member responsibilities were to help students in project planning, review issues and pull requests, ensure smooth progress and help juniors out in their difficulties.",
+    //       color: "#4285F4",
+    //     },
+    //   ],
+    // },
+  ],
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects, Python Scripts, and React Project. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+  avatar_image_path: "projects_image.svg",
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "profile_pic.jpg",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+  blogSection: {
+    title: "Blogs",
+    subtitle:
+      "I don't blog frequently but when I do something awesome, I do try to document it so it can be helpful to others.",
+    link: "https://medium.com/@vedanshvijay/",
+    avatar_image_path: "blogs_image.svg",
+  },
+};
+
+const projects = {
+  data: [
+    {
+      id: "0",
+      name: "Onesite Facial Recognition",
+      url: "",
+      description:
+        "Hackathon project that implemented a facial recognition service that is displayed on a React.js application",
+      languages: [
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "Concert Ticket Alert System",
+      url: "",
+      description:
+        "Service developed would take commands, scrape various API's, and give real time information and updates on concert tickets. Utilized by over 600 daily users",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "Policy Recommendation Engine",
+      url: "",
+      description:
+        "Optimized client data through different models such as Linear Models, ChiSQ Tests, and QQPlots. Trained and implemented Machine Learning to statistically align prospective clients with the best policy type. ",
+      languages: [
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Python",
+          iconifyClass: "logos-python",
+        },
+        
+      ],
+    },
+    {
+      id: "15",
+      name: "Portfolio Website",
+      url: "",
+      description: "The Site you are on now!",
+      languages: [
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "ReactJS",
+          iconifyClass: "logos-react",
+        },
+      ],
+    },
+  ],
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
